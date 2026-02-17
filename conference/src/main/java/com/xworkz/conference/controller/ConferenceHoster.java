ab8a75375a3b6fe383ac3055a59f13eccee515c8
manu-005 <manojbetadur@gmail.com>
2026-02-17 22:23:27 +0530

    web initialization and configuration

--- conference/src/main/java/com/xworkz/conference/controller/ConferenceHoster.java
+++ conference/src/main/java/com/xworkz/conference/controller/ConferenceHoster.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 public class ConferenceHoster {
 
 
+
     public ConferenceHoster(){
         System.out.println("conference hoster controller object created ..");
     }
