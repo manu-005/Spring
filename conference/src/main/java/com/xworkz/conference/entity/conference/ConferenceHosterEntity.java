6465512629088f8ddf5d4de17381921703481c76
manu-005 <manojbetadur@gmail.com>
2026-02-21 00:02:16 +0530

    validation in dto

--- conference/src/main/java/com/xworkz/conference/entity/conference/ConferenceHosterEntity.java
+++ conference/src/main/java/com/xworkz/conference/entity/conference/ConferenceHosterEntity.java
@@ -1,6 +1,47 @@
 package com.xworkz.conference.entity.conference;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class ConferenceHosterEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String fullName;
+
+    private String officialEmail;
+
+    private String organizationName;
+
+    private String conferenceTitle;
+
+    private String conferenceDescription;
+
+    private LocalDate date;
+
+    private LocalTime time;
+
+    private String mode;
+
+    private String venueOrMeetingLink;
+
+    // Store file path instead of MultipartFile
+    private String bannerPath;
+
+    @Column(nullable = false)
+    private String promoVideoPath;
 }
